baff6c6a4b4cccb276b0384110017c626a98cb47
Huseyin <huseyin.alecakir@afiniti.com>
2022-05-17 23:55:36 +0300

    Add dns

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,4 +2,4 @@
 set -e
 
 docker pull halecakir/turkish_delight_nlp_api
-docker run -p 8001:8000 -d --name turkish_delight_nlp_api --rm halecakir/turkish_delight_nlp_api
+docker run --dns 8.8.8.8  -p  8001:8000 -d --name turkish_delight_nlp_api --rm halecakir/turkish_delight_nlp_api
